7e00ced6b1ca761825fb8b7557bea546431bf118
marcohald <marcohald@users.noreply.github.com>
2015-08-09 18:33:45 +0200

    updated database model

--- hello/models.py
+++ hello/models.py
@@ -4,13 +4,7 @@ from django.db import models
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
-class Musician(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    instrument = models.CharField(max_length=100)
-
-class Album(models.Model):
-    artist = models.ForeignKey(Musician)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
+class MP-Success(models.Model):
+    link = models.CharField(max_length=100)
+    html = models.CharField(max_length=5000)
+    anschreiben = models.CharField(max_length=500)
