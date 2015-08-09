ba39da4c5fa425abc884a8582efaaa522b746291
marcohald <marcohald@users.noreply.github.com>
2015-08-09 18:37:57 +0200

    changed table name

--- hello/models.py
+++ hello/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
-class MP-Success(models.Model):
+class MP_Success(models.Model):
     link = models.CharField(max_length=100)
     html = models.CharField(max_length=5000)
     anschreiben = models.CharField(max_length=500)
